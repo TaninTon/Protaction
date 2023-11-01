85faf69a24b86ec435af4ea68c16a3a9904df2e8
tonngub <kasius1113@gmail.com>
2023-11-01 10:01:38 +0700

    Add project files.

---
+++ student/BackAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    internal class BackAccount
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private string Password { get; set; }
+        protected double Money { get; set; }
+        internal double Rate { get; set; }
+
+        public void SetPassword(string Password)
+        {
+            this.Password = Password;
+
+            if(this.Password == "1234")
+            {
+                Console.WriteLine(Money);
+            }
+            else
+            {
+                Console.WriteLine(Money);
+            }
+        }
+
+    }
+}
---
+++ student/Program.cs
@@ -0,0 +1,8 @@
+﻿using student;
+
+var account = new BackAccount();
+account.Id = 1;
+account.Name = "Test";
+account.Rate = 10;
+
+account.SetPassword("1234");
