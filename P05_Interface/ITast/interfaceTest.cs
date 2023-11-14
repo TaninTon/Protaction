492e216835f414c01e19c3cc5f1cdab97e9ea029
tonngub <kasius1113@gmail.com>
2023-11-14 14:07:45 +0700

    Interfact_concept

---
+++ P05_Interface/ITast/interfaceTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05_Interface.ITast
+{
+    internal interface Interface1
+    {
+    }
+}
